d2516737e1c1d4468bcf848609ad91032baa17b6
KOBBY_IM <nyarkotheo2@gmail.com>
2024-03-12 23:59:17 +0000

    add check if se is an instanceof Set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,6 +2,10 @@ function cleanSet(set, startString) {
   if (!startString || !startString.trim()) {
     return '';
   }
+  // check if set is an instance of Set
+  if (!(set instanceof Set)) {
+    throw new Error('The "set" argumenent must be a Set');
+  }
   let result = '';
   for (const item of set) {
     if (item && item.startsWith(startString)) {
